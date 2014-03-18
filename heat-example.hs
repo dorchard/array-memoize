fb22e1274e059f502049e761b9d81deac93d46c1
Dominic Orchard <dom.orchard@gmail.com>
2014-03-18 16:53:34 +0000

    some tweaking with direction

--- heat-example.hs
+++ heat-example.hs
@@ -9,14 +9,15 @@ nt = 2.1    :: Float
 
 heat' :: ((Float, Float) -> Float) -> (Float, Float) -> Float
 heat' _ (0.0, t) = 1
+heat' _ (1.1, t) = 1
 heat' _ (x, 0.0) = 0
 heat' h (x, t) = (h_old x) + r * (h_old (x - delt) - 2 * (h_old x) + h_old (x + delx))
-                   where h_old x = h (x, t - delt)
+                   where h_old x = h (x, t + delt)
                          r       = alpha * (delt / (delx * delx))
 
 heat :: (Float, Float) -> Float
 heat = discMemo heat' ((0, 0), (nx, nt)) (delx, delt)
 
-foo = enumFromThenTo (0, 0) (delx, delt) (nx, nt)
+foo = map (\x -> discretize x (delx,delt)) (enumFromThenTo (0, 0) (delx, delt) (nx, nt))
 
 heatA = discMemoA heat' ((0, 0), (nx, nt)) (delx, delt)
