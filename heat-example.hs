3f93b937dab2c9a0703c71457d7794a36758a5d3
Dominic Orchard <dom.orchard@gmail.com>
2014-03-19 17:44:28 +0000

    cleanup

--- heat-example.hs
+++ heat-example.hs
@@ -4,22 +4,22 @@ import Data.Function.ArrayMemoize
 heat :: (Int, Int) -> Double
 heat = let   -- Parameter setup
              alpha = 0.23
-             delt = 0.05 
-             delx = 0.1 
+             dt = 0.05 
+             dx = 0.1 
              nt = 5
              nx = 3
 
-             r = alpha * (delt / (delx * delx))
+             r = alpha * (dt / (dx * dx))
 
              -- Continuous heat equation
              h :: (Double, Double) -> Double
              h (0.0, t) = 1
              h (3.0, t) = 0
              h (x, 0.0) = 0
-             h (x, t)   = h' (x, t-delt) + r * (h' (x-delx, t-delt) - 2*(h' (x, t-delt)) + h' (x+delx, t-delt))
+             h (x, t)   = h' (x, t-dt) + r * (h' (x-dx, t-dt) - 2*(h' (x, t-dt)) + h' (x+dx, t-dt))
 
              -- Faster quantized heat equation
-             h' = quantizedMemo h ((0, 0), (nx, nt)) (delx, delt)
+             h' = quantizedMemo h ((0, 0), (nx, nt)) (dx, dt)
 
         -- Finally return discrete version 
-        in discrete h' (delx, delt)
+        in discrete h' (dx, dt)
